bb7c2e4f3b9c23c02fe290f8853063cc2f7dd5a5
denizt <admin@yokmail.com>
2019-02-04 09:42:25 +0300

    Tested, working, readme updated

--- src/main/java/com/example/plugins/tutorial/jira/jira/jql/RecentProjectFunction.java
+++ src/main/java/com/example/plugins/tutorial/jira/jira/jql/RecentProjectFunction.java
@@ -1,5 +1,10 @@
-package com.example.plugins.tutorial.jira.jira.jql;
+package com.example.plugins.tutorial.jira.jql;
 
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.UserHistoryItem;
+import com.atlassian.jira.user.UserProjectHistoryManager;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.atlassian.jira.JiraDataType;
@@ -8,39 +13,28 @@ import com.atlassian.jira.jql.operand.QueryLiteral;
 import com.atlassian.jira.jql.query.QueryCreationContext;
 import com.atlassian.jira.plugin.jql.function.AbstractJqlFunction;
 import com.atlassian.jira.util.MessageSet;
-import com.atlassian.jira.util.NotNull;
 import com.atlassian.query.clause.TerminalClause;
 import com.atlassian.query.operand.FunctionOperand;
-import com.google.common.collect.Iterables;
-import com.atlassian.jira.user.ApplicationUser;
-import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
-import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import com.atlassian.jira.user.UserHistoryItem;
-import com.atlassian.jira.user.UserProjectHistoryManager;
 import java.util.LinkedList;
-
-import java.util.Collections;
 import java.util.List;
 
-/**
- * Echoes the the string passed in as an argument.
- */
 @Scanned
-public class RecentProjectFunction extends AbstractJqlFunction
-{
+public class RecentProjectFunction extends AbstractJqlFunction {
     private static final Logger log = LoggerFactory.getLogger(RecentProjectFunction.class);
+
     @ComponentImport
     private final UserProjectHistoryManager userProjectHistoryManager;
 
     public RecentProjectFunction(UserProjectHistoryManager userProjectHistoryManager) {
         this.userProjectHistoryManager = userProjectHistoryManager;
     }
+
     public MessageSet validate(ApplicationUser searcher, FunctionOperand operand, TerminalClause terminalClause) {
         return validateNumberOfArgs(operand, 0);
     }
 
     public List<QueryLiteral> getValues(QueryCreationContext queryCreationContext, FunctionOperand operand, TerminalClause terminalClause) {
-        final List<QueryLiteral> literals = new LinkedList<>();
+        final List<QueryLiteral> literals = new LinkedList();
         final List<UserHistoryItem> projects = userProjectHistoryManager.getProjectHistoryWithoutPermissionChecks(queryCreationContext.getApplicationUser());
 
         for (final UserHistoryItem userHistoryItem : projects) {
@@ -52,17 +46,15 @@ public class RecentProjectFunction extends AbstractJqlFunction
                 log.warn(String.format("User history returned a non numeric project IS '%s'.", value));
             }
         }
+
         return literals;
     }
 
-    public int getMinimumNumberOfExpectedArguments()
-    {
+    public int getMinimumNumberOfExpectedArguments() {
         return 0;
     }
 
-    public JiraDataType getDataType()
-    {
+    public JiraDataType getDataType() {
         return JiraDataTypes.PROJECT;
     }
 }
-
